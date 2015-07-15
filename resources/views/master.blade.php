99f538a829fdbb3eca8efd05cdfb77c7f321593b
Kevin Cunanan <kevinc@codeforamerica.org>
2015-07-15 14:16:21 -0700

    Made images web friendly and added google analytics to master.blade.php

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,7 +8,7 @@
     <meta name="author" content="">
 
     <title>@yield('title')</title>
-    
+
     <link rel="shortcut icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
 	  <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,700' rel='stylesheet' type='text/css'>
     <link href="{{ URL::asset('/assets/css/bootstrap.min.css') }}" rel="stylesheet" media="screen">
@@ -66,6 +66,16 @@
    	<script type="text/javascript" src="{{ URL::asset('/assets/js/countdown.js') }}"></script>
     <script type="text/javascript" src="{{ URL::asset('/assets/js/jquery.vegas.min.js') }}"></script>
    	<script type="text/javascript" src="{{ URL::asset('/assets/js/scripts.js') }}"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-65186134-1', 'auto');
+      ga('send', 'pageview');
+      
+    </script>
 </body>
 
 </html>
